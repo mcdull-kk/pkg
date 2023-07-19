7ad146a335cfa412bdea476f300df241be92f965
yangleizhou <yangleizhou@xunlei.com>
2023-07-19 19:58:50 +0800

    apollo

--- config/apollo/watcher.go
+++ config/apollo/watcher.go
@@ -1,1 +1,23 @@
 package apollo
+
+import (
+	"context"
+
+	"github.com/mcdull-kk/pkg/config"
+)
+
+type (
+	watcher struct {
+		out <-chan []*config.KeyValue
+
+		ctx      context.Context
+		cancelFn func()
+	}
+
+	changeListener struct {
+		in     chan<- []*config.KeyValue
+		apollo *apollo
+	}
+)
+
+func newWatcher(a *apollo)
